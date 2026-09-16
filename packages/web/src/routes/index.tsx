c8ad23f8b95ad0fe95e8eba8624f57e9624fc254
mhmadamrii <interceptorghost4@gmail.com>
2026-09-16 14:37:41 +0700

    feat(web): port from react to solid

---
+++ packages/web/src/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main class="flex min-h-screen items-center justify-center">
+      <h1 class="text-4xl font-bold text-blue-600">Hello world!</h1>
+    </main>
+  );
+}

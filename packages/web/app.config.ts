fad55c62ca22b4c0ea7dd8cc9086fa88b63ca8b3
mhmadamrii <interceptorghost4@gmail.com>
2026-09-21 19:08:04 +0700

    feat&chore(cf): deploy solidstart to cloudflare

--- packages/web/app.config.ts
+++ packages/web/app.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare-module",
+  },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
